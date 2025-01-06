8d7eef064475ee19ddc05538b5716eecde5e86ec
MishkaKovalQA <mishka.koval.qa@gmail.com>
2025-01-06 16:58:23 +0500

    add QaGuruLoginTests

---
+++ src/test/java/qa/qa_guru/QaGuruLoginTests.java
@@ -0,0 +1,78 @@
+package qa.qa_guru;
+
+import com.codeborne.selenide.Configuration;
+import domain.Browser;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import qa.BaseTest;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class QaGuruLoginTests extends BaseTest {
+
+    public static final String LOGIN_PAGE = "https://school.qa.guru/cms/system/login";
+    public static final String EMAIL = "delienfx@gmail.com";
+    public static final String VALID_PASSWORD = "34+DCe3AD";
+
+    @Execution(ExecutionMode.SAME_THREAD)
+    @ParameterizedTest
+    @EnumSource(value = Browser.class)
+    void successfulLoginWithEnterTest(Browser browser) {
+        Configuration.browser = browser.getName();
+
+        open(LOGIN_PAGE);
+        checkLoginForm();
+
+        fillLoginFormByPassword(VALID_PASSWORD);
+        $("[name=password]").pressEnter();
+        checkLoginedForm();
+
+        logoutWithButton();
+        checkLoginForm();
+    }
+
+    @Test
+    void successfulLoginWithSubmitButtonTest() {
+        open(LOGIN_PAGE);
+
+        fillLoginFormByPassword(VALID_PASSWORD);
+        loginWithButton();
+        checkLoginedForm();
+    }
+
+    @Test
+    void unsuccessfulLoginTest() {
+        open(LOGIN_PAGE);
+        fillLoginFormByPassword("WRONG");
+        loginWithButton();
+
+        $(".btn-success").shouldHave(text("Неверный пароль"));
+        $(".logined-form").shouldNotHave(text("Здравствуйте"));
+    }
+
+    private void loginWithButton() {
+        $(".btn-success").click();
+    }
+
+    private void logoutWithButton() {
+        $(".btn-logout").click();
+    }
+
+    private void fillLoginFormByPassword(String password) {
+        $("[name=email]").setValue(EMAIL);
+        $("[name=password]").setValue(password);
+    }
+
+    private void checkLoginForm() {
+        $(".login-form").shouldHave(text("Войти"));
+    }
+
+    private static void checkLoginedForm() {
+        $(".logined-form").shouldHave(text("Здравствуйте"));
+    }
+}
