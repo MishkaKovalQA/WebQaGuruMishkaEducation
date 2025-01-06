ca3f1f5930f1d8c8d339959820e26da38320c36e
MishkaKovalQA <mishka.koval.qa@gmail.com>
2025-01-06 16:57:43 +0500

    add lombok and parallel execution possibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("io.freefair.lombok") version "8.11"
 }
 
 group = "qa.guru.edu"
@@ -11,10 +12,21 @@ repositories {
 
 dependencies {
     testImplementation("com.codeborne:selenide:7.6.1")
-    testImplementation(platform("org.junit:junit-bom:5.11.4"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
+    testImplementation("ch.qos.logback:logback-classic:1.5.15")
 }
 
 tasks.test {
     useJUnitPlatform()
+    systemProperty("junit.jupiter.execution.parallel.enabled", "true")
+    systemProperty("junit.jupiter.execution.parallel.mode.default", "concurrent")
+    systemProperty("junit.jupiter.execution.parallel.mode.classes.default", "concurrent")
+    systemProperty("junit.jupiter.execution.parallel.config.strategy", "fixed")
+    systemProperty("junit.jupiter.execution.parallel.config.fixed.parallelism", "5")
+
+    testLogging {
+        events("passed", "skipped", "failed")
+        showStandardStreams = true
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+    }
 }
