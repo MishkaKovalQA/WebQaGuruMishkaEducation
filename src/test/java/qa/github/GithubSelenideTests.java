b10d496ed98ab80aa8c399a51665a3468eb0eb66
MishkaKovalQA <mishka.koval.qa@gmail.com>
2025-01-06 16:58:55 +0500

    add GoogleSearchTests

--- src/test/java/qa/github/GithubSelenideTests.java
+++ src/test/java/qa/github/GithubSelenideTests.java
@@ -1,4 +1,4 @@
-package github;
+package qa.github;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
---
+++ src/test/java/qa/google/GoogleSearchTests.java
@@ -0,0 +1,34 @@
+package qa.google;
+
+import org.junit.jupiter.api.Test;
+import qa.BaseTest;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class GoogleSearchTests extends BaseTest {
+
+    @Test
+    void successfulSearchWithSubmitButtonTest() {
+        openGoogleSite();
+        $("[name=q]").setValue("Selenide");
+        $("[name=btnK]").click();
+        checkSearchResult();
+    }
+
+    @Test
+    void successfulSearchWithEnterTest() {
+        openGoogleSite();
+        $("[name=q]").setValue("Selenide").pressEnter();
+        checkSearchResult();
+    }
+
+    private void checkSearchResult() {
+        $("#search").shouldHave(text("https://ru.selenide.org"));
+    }
+
+    private void openGoogleSite() {
+        open("https://www.google.com");
+    }
+}
