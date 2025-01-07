27ac44f0243cf62d9d2977ee0036e37a639ac2ce
MishkaKovalQA <mishka.koval.qa@gmail.com>
2025-01-08 02:29:44 +0500

    refactor BeforeEach into BeforeAll

--- src/test/java/qa/BaseTest.java
+++ src/test/java/qa/BaseTest.java
@@ -2,18 +2,19 @@ package qa;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 
 import static com.codeborne.selenide.Selenide.clearBrowserCookies;
 import static com.codeborne.selenide.Selenide.clearBrowserLocalStorage;
 
 public abstract class BaseTest {
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    static void setUp() {
         Configuration.browserCapabilities.setCapability("pageLoadStrategy", "eager");
         Configuration.pageLoadTimeout = 60000;
         Configuration.timeout = 10000;
+        Configuration.browserSize = "1920x1080";
     }
 
     @AfterEach
