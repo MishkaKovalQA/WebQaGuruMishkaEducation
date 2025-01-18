8fdd1c9eb262d9787685ddcab9ccd1f318e9326d
MishkaKovalQA <mishka.koval.qa@gmail.com>
2025-01-18 20:29:07 +0500

    add allure to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,25 @@
 plugins {
     id("java")
     id("io.freefair.lombok") version "8.11"
+    id("io.qameta.allure") version "2.12.0"
 }
 
 group = "qa.guru.edu"
 version = "1.0-SNAPSHOT"
+
+allure {
+    report {
+        version.set("2.24.0") //allure report framework 2
+    }
+    adapter { //generate folder allure-results
+        aspectjWeaver.set(true) // handles annotation step
+        frameworks {
+            junit5 {
+                adapterVersion.set("2.24.0")
+            }
+        }
+    }
+}
 
 repositories {
     mavenCentral()
@@ -12,11 +27,8 @@ repositories {
 
 dependencies {
     testImplementation("com.codeborne:selenide:7.6.1")
-
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
-
     testImplementation("ch.qos.logback:logback-classic:1.5.16")
-
     testImplementation("org.assertj:assertj-core:3.27.2")
 
     implementation("com.google.code.gson:gson:2.11.0")
@@ -26,6 +38,8 @@ dependencies {
     implementation("com.opencsv:opencsv:5.9")
 
     implementation("com.github.javafaker:javafaker:1.0.2")
+
+    implementation("io.qameta.allure:allure-selenide:2.29.1")
 }
 
 tasks.test {
