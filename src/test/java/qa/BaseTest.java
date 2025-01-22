f833ab6b1043d4144c60e63e64084904d56d3818
MishkaKovalQA <mishka.koval.qa@gmail.com>
2025-01-22 23:43:57 +0500

    add allure notifications for tg channel

--- src/test/java/qa/BaseTest.java
+++ src/test/java/qa/BaseTest.java
@@ -6,7 +6,13 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import qa.demoqa.helper.Attach;
 import qa.demoqa.page.RegistrationPage;
+
+import java.util.Map;
+
+import static com.codeborne.selenide.Browsers.CHROME;
 
 public abstract class BaseTest {
 
@@ -19,17 +25,17 @@ public abstract class BaseTest {
         Configuration.timeout = 10000;
         //write only that for running in selenoid without browser gui
         //bad practice, we need do that from jenkins
-        //Configuration.remote = "http://user1:1234@" + System.getProperty("selenoid_url", "selenoid.autotests.cloud") + ":4444/wd/hub";
-/*        Configuration.browser = System.getProperty("browser_name", CHROME);
+        Configuration.remote = "http://user1:1234@" + System.getProperty("selenoid_url", "selenoid.autotests.cloud") + ":4444/wd/hub";
+        Configuration.browser = System.getProperty("browser_name", CHROME);
         Configuration.browserVersion = System.getProperty("browser_version", "100.0");
-        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");*/
+        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
 
-/*        DesiredCapabilities capabilities = new DesiredCapabilities();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true, //window in window for selenoid
                 "enableVideo", true //capture the video, headless should be false
-        ));*/
-        //Configuration.browserCapabilities = capabilities;
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
 
     @BeforeEach
@@ -39,10 +45,10 @@ public abstract class BaseTest {
 
     @AfterEach
     void tearDown() {
-/*        Attach.screenshotAs("Last screenshot");
+        Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
-        Attach.addVideo();*/
+        Attach.addVideo();
 /*        clearBrowserCookies();
         clearBrowserLocalStorage();*/
     }
