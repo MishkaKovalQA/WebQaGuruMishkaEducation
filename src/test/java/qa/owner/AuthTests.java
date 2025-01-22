8bce50660df89e4bf3964a3bad24c2f3ea38b799
MishkaKovalQA <mishka.koval.qa@gmail.com>
2025-01-22 23:35:24 +0500

    add allure notifications for tg channel

--- src/test/java/qa/owner/AuthTests.java
+++ src/test/java/qa/owner/AuthTests.java
@@ -8,6 +8,7 @@ import qa.owner.config.AuthConfig;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,18 +22,18 @@ public class AuthTests {
         assertThat(config.password()).isEqualTo("123123123");
     }
 
-    //todo fix test
-    @Disabled("Doesn't work")
+    //try it in jenkins
+    @Disabled("for jenkins")
     @Test
     void testAuthWithSecretFile() throws Exception {
-        Path secret = Files.createTempFile("secret", ".properties");
         String content = "username=secret-user\npassword=secret-pass";
-        Files.write(secret, content.getBytes(StandardCharsets.UTF_8));
+        Path propsPath = Paths.get("/tmp/secret.properties");
+        Files.write(propsPath, content.getBytes(StandardCharsets.UTF_8));
 
         AuthConfig config = ConfigFactory.create(AuthConfig.class, System.getProperties());
 
         assertThat(config.username()).isEqualTo("secret-user");
         assertThat(config.password()).isEqualTo("secret-pass");
-        Files.delete(secret);
+        Files.delete(propsPath);
     }
 }
